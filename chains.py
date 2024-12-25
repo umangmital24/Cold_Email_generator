8b2bb03bd2236932390f2f8ecf64fd7b20e3b090
Umang Mittal <117918427+umangmital24@users.noreply.github.com>
2024-12-26 02:42:54 +0530

    Add files via upload

---
+++ chains.py
@@ -0,0 +1,56 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Umang Mittal, an undergraduate student from NSUT pursuing B.Tech in CSE. Write a cold email to a potential client showcasing your skills and capabilities in providing tailored solutions using AI and software development. Highlight your ability to integrate and automate business processes effectively, helping enterprises achieve scalability, process optimization, cost reduction, and enhanced efficiency.
+
+            if needed mention your technical expertise in C++, Python, JavaScript,GenAI, data analysis (Pandas, NumPy, Matplotlib, Seaborn), and web development (HTML, CSS, React). Use the following portfolio links to showcase your work and demonstrate your skills: {link_list}. 
+            Adjust the tone to reflect your enthusiasm as a student eager to contribute to real-world challenges.
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
